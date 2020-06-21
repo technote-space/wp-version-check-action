31df6057b1ea3b0b7eaba4ef9bef49a0364d098a
Technote <technote.space@gmail.com>
2020-06-21 15:19:58 +0900

    chore: add type

--- src/constant.ts
+++ src/constant.ts
@@ -35,17 +35,17 @@ export const TARGET_EVENTS = {
 };
 
 export const REPLACE_RULES: Array<(version: string, autoload: string) => { file: string; from: RegExp; to: string }> = [
-  (version): { file: string; from: RegExp; to: string } => ({
+  (version: string): { file: string; from: RegExp; to: string } => ({
     file: 'readme.txt',
     from: /^Stable tag\s*:\s*v?\d+(\.\d+)*$/m,
     to: `Stable tag: ${version}`,
   }),
-  (version): { file: string; from: RegExp; to: string } => ({
+  (version: string): { file: string; from: RegExp; to: string } => ({
     file: 'update.json',
     from: /"version"\s*:\s*"v?\d+(\.\d+)*"\s*(,?)$/m,
     to: `"version": "${version}"$2`,
   }),
-  (version, autoload): { file: string; from: RegExp; to: string } => ({
+  (version: string, autoload: string): { file: string; from: RegExp; to: string } => ({
     file: autoload,
     from: /Version\s*:\s*v?\d+(\.\d+)*$/m,
     to: `Version: ${version}`,
