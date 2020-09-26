52018ffd3979679f87f4c1af16732f0589dbb50a
Technote <technote.space@gmail.com>
2020-09-26 17:16:31 +0900

    fix: broken test

--- __tests__/utils/package.test.ts
+++ __tests__/utils/package.test.ts
@@ -10,6 +10,8 @@ import {
   getApiFixture,
   spyOnStdout,
   stdoutCalledWith,
+  spyOnExportVariable,
+  exportVariableCalledWith,
   setChildProcessParams,
   testFs,
   getOctokit,
@@ -215,6 +217,7 @@ describe('commit', () => {
     process.env.INPUT_COMMIT_DISABLED = 'false';
     setChildProcessParams({stdout: 'master\nfeature/test\n'});
     const mockStdout = spyOnStdout();
+    const mockEnv    = spyOnExportVariable();
 
     nock('https://api.github.com')
       .persist()
@@ -258,9 +261,11 @@ describe('commit', () => {
       '::group::Creating commit... [cd8274d15fa3ae2ab983129fb037999f264ba9a7]',
       '::endgroup::',
       '::group::Updating ref... [heads/master] [7638417db6d59f3c431d3e1f261cc637155684cd]',
-      '::set-env name=GITHUB_SHA::7638417db6d59f3c431d3e1f261cc637155684cd',
       '::endgroup::',
       '::set-output name=sha::7638417db6d59f3c431d3e1f261cc637155684cd',
+    ]);
+    exportVariableCalledWith(mockEnv, [
+      {name: 'GITHUB_SHA', val: '7638417db6d59f3c431d3e1f261cc637155684cd'},
     ]);
   });
 });
