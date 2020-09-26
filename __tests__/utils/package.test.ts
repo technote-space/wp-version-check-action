a9834e4bec9c238540afe3fac8919ef4f18b504e
Technote <technote.space@gmail.com>
2020-09-26 17:16:17 +0900

    feat: use log helper

--- __tests__/utils/package.test.ts
+++ __tests__/utils/package.test.ts
@@ -2,7 +2,7 @@
 import nock from 'nock';
 import path from 'path';
 import {ReplaceResult} from 'replace-in-file';
-import {Logger} from '@technote-space/github-action-helper';
+import {Logger} from '@technote-space/github-action-log-helper';
 import {
   getContext,
   testEnv,
